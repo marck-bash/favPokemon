f88d135063530b180df8f746dc73239a3af23466
Marck Bashaw <marckbashaw@gmail.com>
2024-02-01 19:48:35 -0500

    Pokemon project commit

--- favPokemon.mjs
+++ favPokemon.mjs
@@ -1,3 +1,13 @@
+import readline from "readline";
+
+//function to ask questions
+const rl = readline.createInterface(process.stdin, process.stdout);
+function ask(questionText) {
+  return new Promise((resolve, reject) => {
+    rl.question(questionText, resolve);
+  });
+}
+
 //fetch all pokemon data from pokeapi
 const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0");
 const allPokemon = await response.json();
@@ -5,4 +15,16 @@ const allPokemon = await response.json();
 //convert allPokemon.results from array of objects to array of names
 const pokemonNames = allPokemon.results.map((element) => element.name);
 
+let answer = await ask("What is your Favorite Pokemon? ");
 
+
+
+    if (pokemonNames.includes(answer.toLowerCase())) {        
+        console.log("Your favorite Pokemon is: " + answer);
+    } else {
+        console.log("That's not a real pokemon!");
+    }   
+
+
+  //end your code
+process.exit();
